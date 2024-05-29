39e4398184819960e7106f0c6af79eb81e9e6d70
Josh Reini <joshua.reini@snowflake.com>
2024-05-29 15:28:44 -0400

    separate out feedback definitions to new module

--- llm-eval/common_ui.py
+++ llm-eval/common_ui.py
@@ -15,17 +15,10 @@ from schema import (
     ModelConfig,
 )
 
+# feedback functions
+from feedback import feedbacks
+
 from trulens_eval import Tru
-
-# replicate key for running feedback
-import os
-os.environ["REPLICATE_API_TOKEN"] = "r8_..."
-
-from trulens_eval import Feedback, Select
-from trulens_eval.feedback.provider.hugs import Huggingface
-from trulens_eval.feedback.provider.litellm import LiteLLM
-
-import numpy as np
 
 from trulens_eval import TruCustomApp
 
@@ -135,18 +128,6 @@ def configure_model(*, container, model_config: ModelConfig, key: str, full_widt
         "max_new_tokens": st.session_state.get(MAX_NEW_TOKENS_KEY, model_config.max_new_tokens),
         "use_rag": True,
     }
-
-    # set feedback functions for trulens to use
-    provider = LiteLLM(model_engine="replicate/snowflake/snowflake-arctic-instruct")
-    f_context_relevance = (
-            Feedback(provider.context_relevance_with_cot_reasons, name = "Context Relevance")
-            .on_input()
-            .on(Select.RecordCalls.retrieve_context.rets[1][:].node.text)
-            .aggregate(np.mean) # choose a different aggregation method if you wish
-        )
-    f_criminality_input = Feedback(provider.criminality, name = "Criminality input", higher_is_better=False).on(Select.RecordInput)
-    f_criminality_output = Feedback(provider.criminality, name = "Criminality output", higher_is_better=False).on(Select.Record.app.retrieve_and_generate_stream.rets[:].collect())
-    feedbacks = [f_context_relevance, f_criminality_input, f_criminality_output]
 
     if MODEL_KEY not in st.session_state:
         st.session_state[MODEL_KEY] = model_config.model
