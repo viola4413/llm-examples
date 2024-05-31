7e6e3ea44ea66e69565db6d4df04777f53617d3e
corey <corey.hu@snowflake.com>
2024-05-30 22:48:17 -0700

    move to feedbacks

--- llm-eval/common_ui.py
+++ llm-eval/common_ui.py
@@ -15,12 +15,8 @@ from schema import (
     ModelConfig,
 )
 
-from trulens_eval import Feedback, Select
-from trulens_eval.feedback.provider.litellm import LiteLLM
-
-import numpy as np
-
 # feedback functions
+from feedback import create_feedback_fns
 from trulens_eval import TruCustomApp
 
 generator = StreamGenerator()
@@ -110,20 +106,6 @@ def login():
         st.session_state.user_name = existing or new_user
         st.session_state.admin_mode = admin_mode
         st.rerun()
-
-@st.cache_resource
-def create_feedback_fns():
-    # set feedback functions for trulens to use
-    provider = LiteLLM(model_engine="replicate/snowflake/snowflake-arctic-instruct")
-    f_context_relevance = (
-            Feedback(provider.context_relevance_with_cot_reasons, name = "Context Relevance")
-            .on_input()
-            .on(Select.RecordCalls.retrieve_context.rets[1][:].node.text)
-            .aggregate(np.mean) # choose a different aggregation method if you wish
-        )
-    f_criminality_input = Feedback(provider.criminality, name = "Criminality input", higher_is_better=False).on(Select.RecordInput)
-    f_criminality_output = Feedback(provider.criminality, name = "Criminality output", higher_is_better=False).on(Select.Record.app.retrieve_and_generate_stream.rets[:].collect())
-    return [f_context_relevance, f_criminality_input, f_criminality_output]
 
 @st.cache_resource
 def get_tru_app_id(model: str, temperature: float, top_p: float, max_new_tokens: int, use_rag: bool):
--- llm-eval/feedback.py
+++ llm-eval/feedback.py
@@ -8,25 +8,16 @@ import os
 # replicate key for running feedback
 os.environ["REPLICATE_API_TOKEN"] = st.secrets["REPLICATE_API_TOKEN"]
 
-# set feedback functions for trulens to use
-provider = LiteLLM(model_engine="replicate/snowflake/snowflake-arctic-instruct")
-
-f_context_relevance = (
+@st.cache_resource
+def create_feedback_fns():
+    # set feedback functions for trulens to use
+    provider = LiteLLM(model_engine="replicate/snowflake/snowflake-arctic-instruct")
+    f_context_relevance = (
             Feedback(provider.context_relevance_with_cot_reasons, name = "Context Relevance")
             .on_input()
             .on(Select.RecordCalls.retrieve_context.rets[1][:].node.text)
             .aggregate(np.mean) # choose a different aggregation method if you wish
         )
-
-f_criminality_input = (Feedback(provider.criminality,
-             name = "Criminality input",
-             higher_is_better=False)
-             .on(Select.RecordInput)
-    )
-f_criminality_output = (Feedback(provider.criminality,
-                                 name = "Criminality output",
-                                 higher_is_better=False)
-                                 .on(Select.Record.app.retrieve_and_generate_stream.rets[:].collect())
-    )
-
-feedbacks = [f_context_relevance, f_criminality_input, f_criminality_output]
+    f_criminality_input = Feedback(provider.criminality, name = "Criminality input", higher_is_better=False).on(Select.RecordInput)
+    f_criminality_output = Feedback(provider.criminality, name = "Criminality output", higher_is_better=False).on(Select.Record.app.retrieve_and_generate_stream.rets[:].collect())
+    return [f_context_relevance, f_criminality_input, f_criminality_output]
