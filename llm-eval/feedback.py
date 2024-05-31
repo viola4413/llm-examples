e16282be2950978b9af1178eafca9813f057ea83
Josh Reini <joshua.reini@snowflake.com>
2024-05-31 11:19:50 -0400

    feedback update

--- llm-eval/feedback.py
+++ llm-eval/feedback.py
@@ -5,8 +5,8 @@ import streamlit as st
 import numpy as np
 import os
 
-# replicate key for running feedback
-os.environ["REPLICATE_API_TOKEN"] = st.secrets["REPLICATE_API_TOKEN"]
+from dotenv import load_dotenv
+load_dotenv()
 
 @st.cache_resource
 def create_feedback_fns():
@@ -18,6 +18,21 @@ def create_feedback_fns():
             .on(Select.RecordCalls.retrieve_context.rets[1][:].node.text)
             .aggregate(np.mean) # choose a different aggregation method if you wish
         )
-    f_criminality_input = Feedback(provider.criminality, name = "Criminality input", higher_is_better=False).on(Select.RecordInput)
-    f_criminality_output = Feedback(provider.criminality, name = "Criminality output", higher_is_better=False).on(Select.Record.app.retrieve_and_generate_stream.rets[:].collect())
-    return [f_context_relevance, f_criminality_input, f_criminality_output]
+    f_answer_relevance = (
+            Feedback(provider.relevance_with_cot_reasons, name = "Answer Relevance")
+            .on_input()
+            .on(Select.Record.app.retrieve_and_generate_stream.rets[:].collect())
+        )
+    f_criminality_input = (
+        Feedback(provider.criminality_with_cot_reasons,
+                 name = "Criminality input",
+                 higher_is_better=False)
+                 .on(Select.RecordInput)
+    )
+    f_criminality_output = (
+        Feedback(provider.criminality_with_cot_reasons,
+                 name = "Criminality output",
+                 higher_is_better=False)
+                 .on(Select.Record.app.retrieve_and_generate_stream.rets[:].collect())
+    )
+    return [f_context_relevance, f_answer_relevance, f_criminality_input, f_criminality_output]
